a771c5a734d00535afeddee06a6274b1a57f8895
pablomaruottolo <pablogabriel.maruottolo@cieep.com>
2023-09-26 14:29:33 +0200

    actividad 26 sept PMDM

--- app/src/main/java/pablo/maruottolo/ejercicio1pmdm/ContarLetrasYPalabrasActivity.java
+++ app/src/main/java/pablo/maruottolo/ejercicio1pmdm/ContarLetrasYPalabrasActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ContarLetrasActivity extends AppCompatActivity {
+public class ContarLetrasYPalabrasActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
