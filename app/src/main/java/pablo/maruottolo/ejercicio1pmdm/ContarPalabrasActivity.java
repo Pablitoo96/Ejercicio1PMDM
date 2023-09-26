545119673511b127867b60975553ddbdab7c5d95
pablomaruottolo <pablogabriel.maruottolo@cieep.com>
2023-09-26 14:15:00 +0200

    Initial commit

---
+++ app/src/main/java/pablo/maruottolo/ejercicio1pmdm/ContarPalabrasActivity.java
@@ -0,0 +1,14 @@
+package pablo.maruottolo.ejercicio1pmdm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContarPalabrasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contar_palabras);
+    }
+}
